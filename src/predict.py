6a67dbdab5231a658a2eb52682b5bb155270410f
mr-octopus-007 <keyurpf@gmail.com>
2026-08-07 00:32:02 +0530

    added evaluate and predict py files in src, also added some notebook sections

--- src/predict.py
+++ src/predict.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 import os
 import mlflow
 import pandas as pd
@@ -9,20 +10,16 @@ import logging
 mlflow.set_tracking_uri('sqlite:///mlflow.db')
 logging.basicConfig(filename="logs\\process_exec.log", level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
 
-def evaluate(X_raw,m_type='latest'):
+def predict(X_raw,m_type="latest"):
     """Gets the current model marked as 'Champion', i.e., best model
-    and evaluates the model performance on labeled data passed as input"""
+    and predicts labels"""
 
-    logging.info("separating feature columns and target column from the dataframe")
-    X_raw_features = X_raw.drop(columns=['Transported'])
-    y_orig = X_raw.Transported
-
-    logging.info("preprocessing the feature dataframe to get X_processed")
-    X_processed = preprocess(X_raw_features)
+    logging.info("preprocessing the raw dataframe to get X_processed")
+    X_processed = preprocess(X_raw)
 
     if m_type == 'champion':
-        logging.info("loading 'Champion' model from mlflow registry")
-        loaded_model = mlflow.sklearn.load_model("models:/Champion/latest")
+            logging.info("loading 'Champion' model from mlflow registry")
+            loaded_model = mlflow.sklearn.load_model("models:/Champion/latest")
     else:
         logging.info("loading 'latest' model from mlflow runs")
         latest_run = mlflow.search_runs(
@@ -39,27 +36,24 @@ def evaluate(X_raw,m_type='latest'):
     current_threshold = 0.5584083119472486
     y_pred = y_prob[:,1] > current_threshold
 
-    logging.info("calculating performance metrics")
-    
-    result = {
-        'accuracy_score': accuracy_score(y_orig,y_pred),
-        'roc_auc_score': roc_auc_score(y_orig,y_prob[:,1])
-    }
+    logging.info("using y_pred to create an output dataframe")
+    result_df = pd.DataFrame({
+        "PassengerId": X_raw["PassengerId"],
+        "Transported": y_pred
+    })
 
-    logging.info(f"accuracy_score is {result['accuracy_score']}, and roc_auc_score is {result['roc_auc_score']}")
-
-    return result
+    return result_df
 
 
 if __name__ == "__main__":
 
     arg_parser = ArgumentParser()
-    arg_parser.add_argument("--raw_file_name",type=str,default="train.csv")
+    arg_parser.add_argument("--raw_file_name",type=str,default="test.csv")
     arg_parser.add_argument("--m_type",type=str,default="latest")
 
     args = arg_parser.parse_args()
 
-    logging.info("starting evaluate script")
+    logging.info("starting predict script")
 
     input_dir = "data/raw/"
     os.makedirs(input_dir, exist_ok=True)
@@ -76,4 +70,15 @@ if __name__ == "__main__":
     logging.info(f"csv file loaded successfully from path: {path_to_raw_data}")
 
     logging.info(f"calling evaluate function with this raw csv file as input df")
-    eval_res = evaluate(X,args.m_type)
+    result_df = predict(X,args.m_type)
+
+    # Get current date and time, and Format as yyyymmdd_hhmmss
+    now = datetime.now()
+    timestamp_str = now.strftime("%Y%m%d_%H%M%S")
+
+    output_dir = "data/output/"
+    os.makedirs(output_dir, exist_ok=True)
+    path_to_output_csv = os.path.join(output_dir,f"{args.raw_file_name.split(".")[0]}_{timestamp_str}.csv")
+
+    logging.info(f"coverting result_df into a csv and storing at {path_to_output_csv}")
+    result_df.to_csv(path_to_output_csv, index=False, encoding='utf-8')
